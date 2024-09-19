381dc63d376931a0e92db644faab7084c7dacf44
RemyaManoharan <remyajinil@outlook.com>
2024-09-19 10:16:13 +0200

    string formatting

---
+++ StringFormat.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TestProject   
+{   
+    public class StringFormat
+    {
+        public static void StringFormatApp()            
+        {
+
+        }
+        }
+}
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4c537c9ff22877c41fc695d7cbbef4236b21d37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db42feaedf8b1f38a85ec97cb3e55d66fefd6284")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
